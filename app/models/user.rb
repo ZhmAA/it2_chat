605b491af1ec62b27225428657d7fb55d9ee5a47
ZhmAA <azhm@ukr.net>
2016-11-10 00:16:59 +0200

    add online/offline status

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
   has_and_belongs_to_many :chats
   has_many :messages, dependent: :destroy
-  
+
   def name
     email.split('@')[0]
+  end
+
+  def online?
+    updated_at > 5.minutes.ago
   end
 end
